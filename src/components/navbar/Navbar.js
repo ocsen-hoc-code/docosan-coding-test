b9451596fe0fdfae0cec0cc21c3a8bef3f0cdb16
Do Lam Binh Minh <minhdlb@blockchainlabs.asia>
2021-01-19 15:15:37 +0700

    Update state

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,13 +3,28 @@ import { useSelector, useDispatch } from 'react-redux';
 import { updateList, doctors } from '../../reducers/Doctor/doctorReducer'
 
 const Navbar = () => {
-    const doctorsList = useSelector(doctors);
     const dispatch = useDispatch();
     const [order, setOrder] = useState({ value: 'distance', text: 'Khoảng cách' });
     const [language, setLanguage] = useState({ value: null, text: 'Ngôn ngữ' });
 
+    const doctorFilter = () => {
+        
+    }
+
     const clearLanguage = () => {
         setLanguage({ value: null, text: 'Ngôn ngữ' });
+    }
+
+    const updateOrder = (value, text) => {
+        setOrder({ value, text }, () => {
+
+        })
+    }
+
+    const updateLanguage = (value, text) => {
+        setLanguage({ value, text }, () => {
+
+        })
     }
 
     return (
@@ -20,10 +35,10 @@ const Navbar = () => {
                     {order.text}
                 </button>
                 <ul class="dropdown-menu dropdown-item ">
-                    <li onClick={() => { setOrder({ value: 'distance', text: 'Khoảng cách' }) }}>
+                    <li onClick={() => { updateOrder('distance', 'Khoảng cách') }}>
                         <a href="#"><input class="dropdown-radio-button" type="radio" checked={order.value == "distance"} /> Khoảng cách</a>
                     </li>
-                    <li onClick={() => { setOrder({ value: 'rating', text: 'Đánh giá' }) }}>
+                    <li onClick={() => { updateOrder('rating', 'Đánh giá') }}>
                         <a href="#"><input class="dropdown-radio-button" type="radio" checked={order.value == "rating"} /> Đánh giá</a>
                     </li>
                 </ul>
@@ -35,13 +50,13 @@ const Navbar = () => {
                     <span class="dropdown-clear" onClick={() => { clearLanguage() }} style={{ display: null == language.value ? "none" : "block" }}>X</span>
                 </button>
                 <ul class="dropdown-menu dropdown-item ">
-                    <li onClick={() => { setLanguage({ value: 'vi', text: 'Tiếng việt' }) }}>
+                    <li onClick={() => { updateLanguage('vi', 'Tiếng việt') }}>
                         <a href="#"><input class="dropdown-radio-button" type="radio" checked={language.value == "vi"} /> Tiếng việt</a>
                     </li>
-                    <li onClick={() => { setLanguage({ value: 'en', text: 'English' }) }}>
+                    <li onClick={() => { updateLanguage('en', 'English') }}>
                         <a href="#"><input class="dropdown-radio-button" type="radio" checked={language.value == "en"} /> English</a>
                     </li>
-                    <li onClick={() => { setLanguage({ value: 'fr', text: 'Francaise' }) }}>
+                    <li onClick={() => { updateLanguage('fr', 'Francaise') }}>
                         <a href="#"><input class="dropdown-radio-button" type="radio" checked={language.value == "fr"} /> Francaise</a>
                     </li>
                 </ul>
