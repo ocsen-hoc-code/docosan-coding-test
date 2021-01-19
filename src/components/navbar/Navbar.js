fafc41c2eea624382a162f6e81df1ff80d79682c
Do Lam Binh Minh <minhdlb@blockchainlabs.asia>
2021-01-19 14:27:52 +0700

    Update UI Navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,13 +5,32 @@ import { updateList, doctors } from '../../reducers/Doctor/doctorReducer'
 const Navbar = () => {
     const doctorsList = useSelector(doctors);
     const dispatch = useDispatch();
+    const [order, setOrder] = useState({ value: 'distance', text: 'Khoảng cách' });
+    const [language, setLanguage] = useState({ value: null, text: 'Ngôn ngữ' });
 
     return (
         <div>
             <span class="title">Sắp xếp theo </span>
-            <span class="title navbar">Khoảng cách</span>
+            <div class="dropdown">
+                <button class="title navbar dropdown-toggle" type="button" data-toggle="dropdown">
+                    {order.text}
+                </button>
+                <ul class="dropdown-menu dropdown-item ">
+                    <li><a href="#">Khoảng cách</a></li>
+                    <li><a href="#">Đánh giá</a></li>
+                </ul>
+            </div>
             <span class="title">Lọc kết quả </span>
-            <span class="title navbar">Ngôn ngữ</span>
+            <div class="dropdown">
+                <button class="title navbar dropdown-toggle" type="button" data-toggle="dropdown">
+                    {language.text} <span class="dropdown-clear" style={{ display: null == language.value ? "none" : "block" }}>X</span>
+                </button>
+                <ul class="dropdown-menu dropdown-item ">
+                    <li><a href="#"><input class="dropdown-radio-button" type="radio" checked="true"/> Tiếng việt</a></li>
+                    <li><a href="#"><input class="dropdown-radio-button" type="radio" /> English</a></li>
+                    <li><a href="#"><input class="dropdown-radio-button" type="radio" /> Francaise</a></li>
+                </ul>
+            </div>
         </div>
     )
 }
