db8b80acc3d647dd7729739065c2a459e0a520dc
Do Lam Binh Minh <minhdlb@blockchainlabs.asia>
2021-01-19 14:48:58 +0700

    Update Navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -8,6 +8,10 @@ const Navbar = () => {
     const [order, setOrder] = useState({ value: 'distance', text: 'Khoảng cách' });
     const [language, setLanguage] = useState({ value: null, text: 'Ngôn ngữ' });
 
+    const clearLanguage = () => {
+        setLanguage({ value: null, text: 'Ngôn ngữ' });
+    }
+
     return (
         <div>
             <span class="title">Sắp xếp theo </span>
@@ -16,19 +20,30 @@ const Navbar = () => {
                     {order.text}
                 </button>
                 <ul class="dropdown-menu dropdown-item ">
-                    <li><a href="#">Khoảng cách</a></li>
-                    <li><a href="#">Đánh giá</a></li>
+                    <li onClick={() => { setOrder({ value: 'distance', text: 'Khoảng cách' }) }}>
+                        <a href="#"><input class="dropdown-radio-button" type="radio" checked={order.value == "distance"} /> Khoảng cách</a>
+                    </li>
+                    <li onClick={() => { setOrder({ value: 'rating', text: 'Đánh giá' }) }}>
+                        <a href="#"><input class="dropdown-radio-button" type="radio" checked={order.value == "rating"} /> Đánh giá</a>
+                    </li>
                 </ul>
             </div>
             <span class="title">Lọc kết quả </span>
             <div class="dropdown">
                 <button class="title navbar dropdown-toggle" type="button" data-toggle="dropdown">
-                    {language.text} <span class="dropdown-clear" style={{ display: null == language.value ? "none" : "block" }}>X</span>
+                    {language.text}
+                    <span class="dropdown-clear" onClick={() => { clearLanguage() }} style={{ display: null == language.value ? "none" : "block" }}>X</span>
                 </button>
                 <ul class="dropdown-menu dropdown-item ">
-                    <li><a href="#"><input class="dropdown-radio-button" type="radio" checked="true"/> Tiếng việt</a></li>
-                    <li><a href="#"><input class="dropdown-radio-button" type="radio" /> English</a></li>
-                    <li><a href="#"><input class="dropdown-radio-button" type="radio" /> Francaise</a></li>
+                    <li onClick={() => { setLanguage({ value: 'vi', text: 'Tiếng việt' }) }}>
+                        <a href="#"><input class="dropdown-radio-button" type="radio" checked={language.value == "vi"} /> Tiếng việt</a>
+                    </li>
+                    <li onClick={() => { setLanguage({ value: 'en', text: 'English' }) }}>
+                        <a href="#"><input class="dropdown-radio-button" type="radio" checked={language.value == "en"} /> English</a>
+                    </li>
+                    <li onClick={() => { setLanguage({ value: 'fr', text: 'Francaise' }) }}>
+                        <a href="#"><input class="dropdown-radio-button" type="radio" checked={language.value == "fr"} /> Francaise</a>
+                    </li>
                 </ul>
             </div>
         </div>
